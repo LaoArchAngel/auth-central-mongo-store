1e251e4cc2e5caf507041b9ec9a39042669ee621
Malick McGregor <MMcGregor@fsw.com>
2016-02-04 16:46:58 -0700

    Initial add of MembershipReboot mongo store.

---
+++ Source/AuthCentral.MongoStore.MembershipReboot/MongoDb/MongoDatabase.cs
@@ -0,0 +1,85 @@
+﻿using BrockAllen.MembershipReboot;
+using BrockAllen.MembershipReboot.Hierarchical;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+
+namespace Fsw.Enterprise.AuthCentral.MongoDb
+{
+    /// <summary>
+    ///     Wrapper for the mongo database, supplying access to the Users and Groups collections.
+    /// </summary>
+    public class MongoDatabase
+    {
+        private readonly string _connectionString;
+
+        static MongoDatabase()
+        {
+            BsonClassMap.RegisterClassMap<Group>(cm =>
+            {
+                cm.AutoMap();
+                cm.MapIdProperty(c => c.ID);
+            });
+
+            BsonClassMap.RegisterClassMap<UserAccount>(cm =>
+            {
+                cm.AutoMap();
+                cm.MapIdProperty(c => c.ID);
+            });
+
+            BsonClassMap.RegisterClassMap<HierarchicalUserAccount>(cm => cm.AutoMap());
+            BsonClassMap.RegisterClassMap<HierarchicalGroup>(cm => cm.AutoMap());
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="MongoDatabase" /> class.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        public MongoDatabase(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        /// <summary>
+        ///     Gets the 'groups' collection from Mongo and coverts them to <see cref="HierarchicalGroup" /> objects.
+        /// </summary>
+        /// <returns><see cref="MongoCollection{HierarchicalGroup}" /> of type <see cref="HierarchicalGroup" /></returns>
+        public MongoCollection<HierarchicalGroup> Groups()
+        {
+            return GetCollection<HierarchicalGroup>("groups");
+        }
+
+        /// <summary>
+        ///     Gets the 'users' collection from Mongo and converts them to <see cref="HierarchicalUserAccount" /> objects.
+        /// </summary>
+        /// <returns>
+        ///     <see cref="MongoCollection{HierarchicalUserAccount}" /> of type <see cref="HierarchicalUserAccount" />
+        /// </returns>
+        public MongoCollection<HierarchicalUserAccount> Users()
+        {
+            return GetCollection<HierarchicalUserAccount>("users");
+        }
+
+        /// <summary>
+        ///     Gets a collection with the given <paramref name="name" /> and converts them to objects of type
+        ///     <typeparamref name="T" />
+        /// </summary>
+        /// <typeparam name="T">Type to serialize the documents to from the mongo collection</typeparam>
+        /// <param name="name">The name of the mongo collection.</param>
+        /// <returns>A <see cref="MongoCollection{TDefaultDocument}" /> of type <typeparamref name="T" /></returns>
+        public MongoCollection<T> GetCollection<T>(string name)
+        {
+            string databaseName = MongoUrl.Create(_connectionString).DatabaseName;
+            var client = new MongoClient(_connectionString);
+
+            // "GetServer(MongoClient) is obsolete: 'Use the new API instead.'"
+            // The new API appears to be stricly asynchronous, 
+            // which is not supported by upstream dependencies
+#pragma warning disable 618
+            MongoServer server = client.GetServer();
+#pragma warning restore 618
+
+            MongoDB.Driver.MongoDatabase database = server.GetDatabase(databaseName);
+            return database.GetCollection<T>(name);
+        }
+    }
+}
---
+++ Source/AuthCentral.MongoStore.MembershipReboot/MongoDb/MongoUserAccountRepository.cs
@@ -0,0 +1,207 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BrockAllen.MembershipReboot;
+using BrockAllen.MembershipReboot.Hierarchical;
+using Fsw.Enterprise.AuthCentral.IdMgr;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+
+namespace Fsw.Enterprise.AuthCentral.MongoDb
+{
+    /// <summary>
+    /// Repository for MembershipReboot UserAccount objects from a Mongo data source.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class MongoUserAccountRepository<T> : QueryableUserAccountRepository<T>, IBulkUserRepository<T>
+        where T : HierarchicalUserAccount, new()
+    {
+        private readonly MongoDatabase _db;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MongoUserAccountRepository{T}"/> class.
+        /// </summary>
+        /// <param name="db">The database.</param>
+        public MongoUserAccountRepository(MongoDatabase db)
+        {
+            UseEqualsOrdinalIgnoreCaseForQueries = true;
+            _db = db;
+        }
+
+        /// <summary>
+        /// Gets a direct queryable of the Users table.
+        /// </summary>
+        /// <value>
+        /// The queryable.
+        /// </value>
+        protected override IQueryable<T> Queryable
+        {
+            get { return (IQueryable<T>) _db.Users().FindAll().AsQueryable(); }
+        }
+
+        /// <summary>
+        /// Gets a paged list of users
+        /// </summary>
+        /// <param name="page">The page of users to get.</param>
+        /// <param name="pageSize">Size of the page.</param>
+        /// <param name="count">The number of users in the page.</param>
+        /// <returns></returns>
+        public IEnumerable<T> GetPagedUsers(int page, int pageSize, out long count)
+        {
+            // Optional parameters must appear after all other parameters
+            if (page < 1)
+            {
+                page = 1;
+            }
+            if (pageSize < 1)
+            {
+                pageSize = 25;
+            }
+
+            MongoCursor<T> accountsQuery = _db.Users().FindAllAs<T>();
+
+            // MongoCursor.Limit(x) restricts how many records can be retrieved
+            // But MongoCursor.Skip enumerates the list, removes the first N and returns the rest as an IEnumerable<T>
+            // This enumeration counts towards Limit. Once Skip is called, Limit is no longer useful.
+            // Therefore to our Limit we have to add the number of records that will be skipped.
+            accountsQuery.Limit = page*pageSize;
+            // Count() locks the cursor, so Limit can't be called anymore
+            count = accountsQuery.Count();
+            IEnumerable<T> accounts = accountsQuery.Skip((page - 1)*pageSize);
+
+            return accounts;
+        }
+
+        /// <summary>
+        /// Creates a blank <see cref="UserAccount"/>
+        /// </summary>
+        /// <returns>New <see cref="UserAccount"/></returns>
+        public override T Create()
+        {
+            return new T();
+        }
+
+        /// <summary>
+        /// Adds the specified <see cref="UserAccount"/> to the repository.
+        /// </summary>
+        /// <param name="item">The <see cref="UserAccount"/> to add to the repository.</param>
+        public override void Add(T item)
+        {
+            _db.Users().Insert(item);
+        }
+
+        /// <summary>
+        /// Updates the specified <see cref="UserAccount"/> that already exists in the repository.  Matches by the unique ID. 
+        /// </summary>
+        /// <param name="item">The <see cref="UserAccount"/> with changes.</param>
+        public override void Update(T item)
+        {
+            _db.Users().Save(item);
+        }
+
+        /// <summary>
+        /// Removes the specified <see cref="UserAccount"/>.  Only the identitfier need be filled.
+        /// </summary>
+        /// <param name="item">The <see cref="UserAccount"/> to remove.</param>
+        public override void Remove(T item)
+        {
+            _db.Users().Remove(Query<T>.EQ(e => e.ID, item.ID));
+        }
+
+        /// <summary>
+        /// Finds an account by a linked account.
+        /// </summary>
+        /// <param name="tenant">The tenant the account we're searching for is in.</param>
+        /// <param name="provider">The name of the provider of the linked account.</param>
+        /// <param name="id">The provider's unique identifier for the linked account.</param>
+        /// <returns>A <see cref="UserAccount"/></returns>
+        public override T GetByLinkedAccount(string tenant, string provider, string id)
+        {
+            IQueryable<T> query =
+                from a in Queryable
+                where a.Tenant == tenant
+                from la in a.LinkedAccountCollection
+                where la.ProviderName == provider && la.ProviderAccountID == id
+                select a;
+            return query.SingleOrDefault();
+        }
+
+        /// <summary>
+        /// Finds an account by their SSL certificate thumbprint.
+        /// </summary>
+        /// <param name="tenant">The tenant.</param>
+        /// <param name="thumbprint">The thumbprint.</param>
+        /// <returns></returns>
+        public override T GetByCertificate(string tenant, string thumbprint)
+        {
+            IQueryable<T> query =
+                from a in Queryable
+                where a.Tenant == tenant
+                from c in a.UserCertificateCollection
+                where c.Thumbprint == thumbprint
+                select a;
+            return query.SingleOrDefault();
+        }
+
+        //void IUserAccountRepository<UserAccount>.Remove(UserAccount item)
+        //}
+        //    This.Add((T)item);
+        //{
+
+        //void IUserAccountRepository<UserAccount>.Add(UserAccount item)
+        //}
+        //    return This.Create();
+        //{
+
+        //UserAccount IUserAccountRepository<UserAccount>.Create()
+
+        //IUserAccountRepository<T> This { get { return (IUserAccountRepository<T>)this; } }
+        //{
+        //    This.Remove((T)item);
+        //}
+
+        //void IUserAccountRepository<UserAccount>.Update(UserAccount item)
+        //{
+        //    This.Update((T)item);
+        //}
+
+        //UserAccount IUserAccountRepository<UserAccount>.GetByID(Guid id)
+        //{
+        //    return This.GetByID(id);
+        //}
+
+        //UserAccount IUserAccountRepository<UserAccount>.GetByUsername(string username)
+        //{
+        //    return This.GetByUsername(username);
+        //}
+
+        //UserAccount IUserAccountRepository<UserAccount>.GetByUsername(string tenant, string username)
+        //{
+        //    return This.GetByUsername(tenant, username);
+        //}
+
+        //UserAccount IUserAccountRepository<UserAccount>.GetByEmail(string tenant, string email)
+        //{
+        //    return This.GetByEmail(tenant, email);
+        //}
+
+        //UserAccount IUserAccountRepository<UserAccount>.GetByMobilePhone(string tenant, string phone)
+        //{
+        //    return This.GetByMobilePhone(tenant, phone);
+        //}
+
+        //UserAccount IUserAccountRepository<UserAccount>.GetByVerificationKey(string key)
+        //{
+        //    return This.GetByVerificationKey(key);
+        //}
+
+        //UserAccount IUserAccountRepository<UserAccount>.GetByLinkedAccount(string tenant, string provider, string id)
+        //{
+        //    return This.GetByLinkedAccount(tenant, provider, id);
+        //}
+
+        //UserAccount IUserAccountRepository<UserAccount>.GetByCertificate(string tenant, string thumbprint)
+        //{
+        //    return This.GetByCertificate(tenant, thumbprint);
+        //}
+    }
+}
