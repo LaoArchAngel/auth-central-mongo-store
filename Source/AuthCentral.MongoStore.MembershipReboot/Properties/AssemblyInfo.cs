70e5f3e8081d6248c891b5d71452c8af01f8dfd2
Malick McGregor <MMcGregor@fsw.com>
2016-02-05 10:41:05 -0700

    Added assembly information for MongoStore.MembershipReboot.

--- Source/AuthCentral.MongoStore.MembershipReboot/Properties/AssemblyInfo.cs
+++ Source/AuthCentral.MongoStore.MembershipReboot/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AuthCentral.MongoStore.MembershipReboot")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MembershipReboot Mongo implementations")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AuthCentral.MongoStore.MembershipReboot")]
+[assembly: AssemblyCompany("FSW")]
+[assembly: AssemblyProduct("AuthCentral")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
